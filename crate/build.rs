eb6eeef4c9ecf8c8607786d9980dd02ec2c52af4
Samuel Kyletoft <samuel@kyletoft.se>
2023-02-16 19:55:15 +0100

    Minimise

--- crate/build.rs
+++ crate/build.rs
@@ -1,33 +1,18 @@
 use std::path::PathBuf;
 
 fn main() -> miette::Result<()> {
-	// s2e subprojects
-	let klee = PathBuf::from("../klee/include");
-	let coroutine = PathBuf::from("../libcoroutine/include");
-	let cpu = PathBuf::from("../libcpu/include");
-	let fsigcxx = PathBuf::from("../libfsigc++/include");
-	let q = PathBuf::from("../libq/include");
-	let s2e = PathBuf::from("../libs2e/include");
-	let s2ecore = PathBuf::from("../libs2ecore/include");
-	let s2eplugins = PathBuf::from("../libs2eplugins/include");
-	let tcg = PathBuf::from("../libtcg/include");
-	let vmi = PathBuf::from("../libvmi/include");
+	let klee = PathBuf::from("../s2e/klee/include");
+	let coroutine = PathBuf::from("../s2e/libcoroutine/include");
+	let cpu = PathBuf::from("../s2e/libcpu/include");
+	let fsigcxx = PathBuf::from("../s2e/libfsigc++/include");
+	let q = PathBuf::from("../s2e/libq/include");
+	let s2e = PathBuf::from("../s2e/libs2e/include");
+	let s2ecore = PathBuf::from("../s2e/libs2ecore/include");
+	let s2eplugins = PathBuf::from("../s2e/libs2eplugins/include");
+	let tcg = PathBuf::from("../s2e/libtcg/include");
+	let vmi = PathBuf::from("../s2e/libvmi/include");
+	let config_host = PathBuf::from("../s2e/tools/lib/X8664BitcodeLibrary");
 
-	// There are 5 files with the same name in s2e. 3 x86 and 2 x86_64
-	// versions. I'm using the first x86_64 version.
-	let config_host = PathBuf::from("../tools/lib/X8664BitcodeLibrary");
-
-	// Dependencies
-	let glibc = PathBuf::from(env!("GLIBC_PATH"));
-	let gcc_libs = PathBuf::from(env!("GCCLIBS_PATH"));
-	let gcc_libs_l = PathBuf::from(env!("GCCLIBS_PATH_L"));
-	let clang_libs = PathBuf::from(env!("CLANGLIBS_PATH"));
-	let boost_libs = PathBuf::from(env!("BOOST_PATH"));
-	let llvm_libs = PathBuf::from(env!("LLVM_PATH"));
-
-	let helpers = PathBuf::from("./cpp_src");
-
-	// Breaks on reordering!!
 	let libraries = [
 		&klee,
 		&coroutine,
@@ -40,13 +25,6 @@ fn main() -> miette::Result<()> {
 		&tcg,
 		&vmi,
 		&config_host,
-		&helpers,
-		&gcc_libs,
-		&gcc_libs_l,
-		&clang_libs,
-		&boost_libs,
-		&llvm_libs,
-		&glibc,
 	];
 
 	autocxx_build::Builder::new(
@@ -62,25 +40,6 @@ fn main() -> miette::Result<()> {
 		"-std=gnu++17",
 	])
 	.build()?;
-
-	cc::Build::new()
-		.cpp(true)
-		.compiler("clang++")
-		.includes(&libraries)
-		.warnings(false)
-		.extra_warnings(false)
-		.flag("-w")
-		.flag("-std=gnu++17")
-		.define("BOOST_BIND_GLOBAL_PLACEHOLDERS", "1")
-		.define("TARGET_PAGE_BITS", "12")
-		.define("SE_RAM_OBJECT_BITS", "12")
-		.define(
-			"SE_RAM_OBJECT_MASK",
-			Some((!11).to_string().as_str()),
-		)
-		.file("cpp_src/RustPlugin.cpp")
-		.file("cpp_src/Helpers.cpp")
-		.compile("rustplugin");
 
 	println!("cargo:rerun-if-changed=src/lib.rs");
 
--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,39 +1,6 @@
 autocxx::include_cpp! {
-	// #include "s2e/CorePlugin.h"
-	// #include "s2e/Plugin.h"
-	// #include "s2e/S2EExecutionState.h"
 	#include "s2e/S2E.h"
-	// #include "RustPlugin.h"
 
 	safety!(unsafe_ffi)
-	// exclude_utilities!()
-	// exclude_impls!()
-	// block!("s2e_S2E_getWarningsStream_autocxx_wrapper")
-
 	generate!("s2e::S2E")
-	// generate!("s2e::Plugin")
-	// generate!("s2e::ConfigFile")
-	// generate!("s2e::ExecutionSignal")
-	// generate!("s2e::S2EExecutionState")
-	// generate!("TranslationBlock")
-	// generate!("s2e::plugins::RustPlugin")
 }
-/*
-pub mod types {
-	pub mod s2e {
-		pub use crate::ffi::s2e::*;
-	}
-
-	pub use crate::ffi::{s2e::plugins::RustPlugin, *};
-}
-
-extern "C" {
-	fn hello_cpp();
-}
-
-pub mod wrappers {
-	pub fn hello_cpp() {
-		unsafe { crate::hello_cpp() }
-	}
-}
-*/
